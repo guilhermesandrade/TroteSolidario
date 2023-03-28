bb06c2c57b76d6ef077ef7834ad3f7db4f4b4f14
Guilherme Andrade <guilhermesantosandrade01@gmail.com>
2023-03-28 14:25:38 -0300

    changes 15 ( points.js adjusts)

--- points/points.js
+++ points/points.js
@@ -1,50 +1,54 @@
-function baskets(){
+function baskets() {
+  let baskets = Number(document.getElementById("baskets").value)
 
-let baskets= Number(document.getElementById("baskets").value)
+  let points
 
-let points
+  if (baskets >= 80) {
+    points = 5000
+  } else if (baskets >= 64 && baskets < 80) {
+    points = 4000
+  } else if (baskets >= 40 && baskets < 64) {
+    points = 2500
+  } else if (baskets >= 16 && baskets < 40) {
+    points = 1000
+  } else if (baskets < 16) {
+    points = 0
+  }
 
-if (baskets >= 80) {
-  points = 5000
-} else if ((baskets >= 64) && (baskets < 80)) {
-
-  points=4000
-} else if ((baskets >=40) && (baskets < 64)) {
-
-  points=2500
-}else if ((baskets>=16) && (baskets<40)){
-
-  points=1000
-}else if(baskets<16){
-
-  points=0
+  document.getElementById("points").innerHTML =
+    "A pontuação foi de " + points + " pontos."
 }
 
-  document.getElementById("points").innerHTML = ("A pontuação foi de " + points + " pontos.")
+function extrabaskets() {
+  let extrabaskets = Number(document.getElementById("extrabaskets").value)
+  let pointsx
+
+  if (extrabaskets >= 0) {
+    pointsx = 50 * extrabaskets
+  } else {
+    pointsx = 0
+  }
+
+  document.getElementById("pointsx").innerHTML =
+    "A pontuação por cestas extras foi de " + pointsx + " pontos"
 }
 
-function extrabaskets(){
+function blood() {
+  let blood = Number(document.getElementById("blood").value)
+  let pointsb
 
-let extrabaskets= Number(document.getElementById("extrabaskets").value)
-let pointsx= 50*extrabaskets
+  if (blood >= 0) {
+    pointsb = blood * 15
+  } else {
+    pointsb = 0
+  }
 
-document.getElementById("pointsx").innerHTML=("A pontuação por cestas extras foi de " + pointsx + " pontos")
-
+  document.getElementById("pointsb").innerHTML =
+    "A pontuação por doação de sangue foi de " + pointsb + " pontos"
 }
 
-function blood(){
+function togglemode() {
+  let html = document.documentElement
 
-  let blood= Number(document.getElementById("blood").value)
-  let pointsb= blood*15
-
-  document.getElementById("pointsb").innerHTML=("A pontuação por doação de sangue foi de " + pointsb + " pontos")
+  html.classList.toggle("light")
 }
-
-function togglemode(){
-
-let html= document.documentElement
-
-html.classList.toggle("light")
-
-}
-
